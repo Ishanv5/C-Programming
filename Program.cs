2475dce55bdf2e46a04ed587dfb938b2dacbf20a
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-18 23:36:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace First
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int a=10;
+            a=20;
+            Console.WriteLine("The value of a: "+a);
+        }
+    }
+}
